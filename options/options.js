b9e9db90811e24862053446583aaff31ab8c0b9c
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2022-05-27 16:19:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "moving_permissions_1",
+  url: "https://moving_permissions_1.botics.co",
+  api: "https://moving_permissions_1.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
